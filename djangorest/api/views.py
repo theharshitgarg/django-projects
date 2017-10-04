9e231e3520e8d4677be35b73f07fe666dbe2f087
harshit garg <harshit@esthenos.com>
2017-10-04 16:29:06 +0530

    add api module

---
+++ djangorest/api/views.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from rest_framework import generics
+
+# Create your views here.
+from .serializers import BucketlistSerializer
+from .models import Bucketlist
+
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new bucketlist."""
+        serializer.save()
+
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class handles the http GET, PUT and DELETE requests."""
+
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
